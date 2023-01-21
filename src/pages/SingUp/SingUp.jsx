7cd7562ff4d3bcc1e24bf4d4f8756aa90915f298
Thamires Pieve <thamires.pieve@gec.inatel.br>
2023-01-21 17:23:07 -0300

    feat: create new user

--- src/pages/SingUp/SingUp.jsx
+++ src/pages/SingUp/SingUp.jsx
@@ -1,4 +1,7 @@
-import {Link} from "react-router-dom"
+import { useState } from "react";
+import {Link, useNavigate} from "react-router-dom"
+import { api }  from '../../services/api'
+
 import { Container,Form,BackgroundImage } from "./style";
 import {FiUser,FiMail,FiLock,FiArrowLeft} from "react-icons/fi"
 
@@ -6,6 +9,31 @@ import { Input } from "../../components/Input/input";
 import { Button } from "../../components/Button/Button";
 
 export function SingUp(){
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigate = useNavigate()
+
+  async function handleSingUp () {
+
+    if(!name || !email || !password){
+      alert('Todos os campos devem ser preenchidos.')
+    }
+
+   await api.post('/user',{name, email, password}).then(()=>{
+      alert('Usuário cadastrado com sucesso')
+      navigate(-1)
+    }).catch((error)=> {
+      console.log(error)
+      if(error.response){
+        alert(error.response.data.message)
+      }else{
+        alert('Desculpe, não foi possível efetuar o cadastro. Tente novamente')
+      }
+    })
+  }
 
   return(
     <Container>
@@ -16,11 +44,11 @@ export function SingUp(){
 
           <h2>Crie sua conta</h2>
 
-          <Input placeholder="Nome" type="text" icon={FiUser}/>
-          <Input placeholder="E-mail" type="email" icon={FiMail}/>
-          <Input placeholder="Senha" type="password" icon={FiLock}/>
+          <Input placeholder="Nome" type="text" icon={FiUser} onChange = {event => setName(event.target.value)} />
+          <Input placeholder="E-mail" type="email" icon={FiMail} onChange = {event => setEmail(event.target.value)} />
+          <Input placeholder="Senha" type="password" icon={FiLock} onChange = {event => setPassword(event.target.value)} />
 
-          <Button type="button" title="Cadastrar"/>
+          <Button type="button" title="Cadastrar" onClick = {handleSingUp} />
 
           <Link to="/"><FiArrowLeft/> Voltar para o login</Link>
       </Form>
